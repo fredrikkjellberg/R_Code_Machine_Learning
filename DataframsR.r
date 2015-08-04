6d89cda93bc0fef76f02681eb21688c7baa7e356
Fredrik Kjellberg <fredrikj@umich.edu>
2015-08-04 18:17:20 -0400

    Update DataframsR.r

--- DataframsR.r
+++ DataframsR.r
@@ -1,1 +1,46 @@
 mtcars # Built-in R data set stored in a data frame
+
+# Have a quick look at your data
+
+#A data frame has the variables of a data set as columns and the observations as rows. This will be a familiar concept for those coming from different statistical software packages such as SAS or SPSS
+
+head(mtcars)
+
+#Another method that is often used to get a rapid overview of your data is the function str(). The function str() shows you the structure of your data set. For a data frame it tells you:
+#The total number of observations (e.g. 32 car types)
+#The total number of variables (e.g. 11 car features)
+#A full list of the variables names (e.g. mpg, cyl … )
+#The data type of each variable (e.g. num for car features)
+#The first observations
+str(mtcars)
+
+#lets create our own data set!
+
+
+planets <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune");
+type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet", "Terrestrial planet", "Gas giant", "Gas giant", "Gas giant", "Gas giant")
+diameter <- c(0.382, 0.949, 1, 0.532, 11.209, 9.449, 4.007, 3.883); 
+rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67);
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE);
+
+# Create the data frame:
+planets_df  <-data.frame(planets,type,diameter,rotation,rings)
+
+
+#Similar to vectors and matrices, you select elements from a data frame with the help of square brackets [ ]. By using a comma, you can indicate what to select from the rows and the columns respectively. For example:
+my_data_frame[1,2] #selects from the first row in my_data_frame the second element.
+my_data_frame[1:3,2:4] #selects rows 1,2,3 and columns 2,3,4 in my_data_frame.
+#Sometimes you want to select all elements of a row or column. To do this, you can use the follwing. my_data_frame[1, ] selects all elements of the first row. Let us now apply this technique on planets_df!
+
+head(planets_df)
+planets_df
+# All data from the first three planets
+closest_planets_df <- planets_df[1:3,]
+
+# All data from the last three planets
+furthest_planets_df <- planets_df[6:8,]
+
+# Have a look:
+closest_planets_df
+furthest_planets_df
+

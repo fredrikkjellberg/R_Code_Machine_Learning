18758513c2c612ca0556f1367042ca3294dbf5cd
Fredrik Kjellberg <fredrikj@umich.edu>
2015-09-01 23:01:21 -0400

    Update Kaggle_R_ML.r
    added answer to data frame issue

--- Kaggle_R_ML.r
+++ Kaggle_R_ML.r
@@ -159,3 +159,16 @@ my_solution <-  data.frame(my_prediction)
 
 # Write your solution to a csv file with the name my_solution.csv
 #write.csv(___, ___ , ___)
+
+
+# Make your prediction using the test set
+my_prediction <- predict(my_tree_two, test, type = "class")
+
+# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+my_solution <- data.frame(PassengerId = test$PassengerId, Survived = my_prediction)
+
+# Check that your data frame has 418 entries
+nrow(my_solution)
+
+# Write your solution to a csv file with the name my_solution.csv
+write.csv(my_solution, file = "my_solution.csv", row.names = FALSE)
